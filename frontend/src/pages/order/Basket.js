b77506a512637e646b6374779d7651c629a1fa0e
pshhhhhhhhhh <winki93@naver.com>
2022-10-25 00:07:57 +0900

    Basket과 OrderForm CSS작업중 각 40 퍼 30퍼 완료
    ㅋ

--- frontend/src/pages/order/Basket.js
+++ frontend/src/pages/order/Basket.js
@@ -1,10 +1,269 @@
+import styled from 'styled-components'; // react에 css 바로 사용 라이브러리
+
 function Basket(){
     return(
-        <h2>SHOPPING CART</h2>
-
-        
+        <Container>
+            <Contents>
+                <Path>현재위치 -- 현재위치</Path>
+                <Title>
+                    <Titletext>SHOPPING CART</Titletext>
+                </Title>
+                <InfoTitleDiv><ControlInfo><ControlInfocontents>국내배송상품</ControlInfocontents></ControlInfo></InfoTitleDiv>
+                <OrderArea>
+                <OrderAreaTitle><OrderAreaTitleContents>일반상품</OrderAreaTitleContents><BeforeButton>이전페이지</BeforeButton></OrderAreaTitle>
+                <InfoTable>
+                    <colgroup>
+                        <col style={{width:"92px"}}></col>
+                        <col style={{width:"auto"}}></col>
+                        <col style={{width:"98px"}}></col>
+                        <col style={{width:"75px"}}></col>
+                        <col style={{width:"98px"}}></col>
+                        <col style={{width:"98px"}}></col>
+                        <col style={{width:"85px"}}></col>
+                        <col style={{width:"98px"}}></col>
+                    </colgroup>
+                    <thead style={{    display: "table-header-group", verticalalign: "middle", bordercolor: "inherit"}}>
+                        <tr style={{    display: "table-row", verticalalign: "inherit", bordercolor: "inherit"}}>
+                            <InfoTh scope='col'>이미지</InfoTh>
+                            <InfoTh scope='col'>상품정보</InfoTh>
+                            <InfoTh scope='col'>판매가</InfoTh>
+                            <InfoTh scope='col'>수량</InfoTh>
+                            <InfoTh scope='col'>적립금</InfoTh>
+                            <InfoTh scope='col'>배송구분</InfoTh>
+                            <InfoTh scope='col'>배송비</InfoTh>
+                            <InfoTh scope='col'>합계</InfoTh>
+                        </tr>
+                    </thead>
+                    <tfoot style={{textalign: "right"}}>
+                        <tr style={{    display: "table-row", verticalalign: "inherit", bordercolor: "inherit", verticalalign:"middle"}}>
+                            <InfoTd colSpan={8}>
+                                <span style={{    float: "left", margin: "6px 0 0"}}>[기본배송]</span>
+                                <Tdcontentstext>상품구매금액</Tdcontentstext>
+                                <Tdcontentstext style={{fontWeight:"bold"}}> 20,000</Tdcontentstext>
+                                <Tdcontentstext>+ 배송비</Tdcontentstext>
+                                <Tdcontentstext  style={{marginLeft:"6px 0 0"}}> 2,500</Tdcontentstext>
+                                <Tdcontentstext> = 합계:</Tdcontentstext>
+                                <Tdcontentstext style={{fontWeight:"bold", fontSize: "18px",letterSpacing: "-1px", marginLeft: "10px" }}>22,500</Tdcontentstext>
+                                <Tdcontentstext style={{fontWeight:"bold", fontSize: "18px",letterSpacing: "-1px"}}>원</Tdcontentstext>
+                             </InfoTd>
+                        </tr>
+                    </tfoot>
+                  
+                    <tbody style={{textalign: "center"}}> 
+                    <tr style={{    display: "table-row", verticalalign: "inherit", bordercolor: "inherit", border:"1"}}>
+                        <InfoTd2><Forimg src="//www.fromdayone.co.kr/web/product/tiny/202112/4b1c9e539d03ec2c7c5d537b1126b100.webp"></Forimg></InfoTd2>
+                        <InfoTd2 style={{paddingLeft: "10px",bordercolor: "#ebebeb",borderRight:"1px solid #ebebeb"}}>
+                        <Tdcontentstext style={{fontWeight:"bold"}}>상품이름상품이름상품이름상품이름상품이름</Tdcontentstext><br></br>
+                        <Tdcontentstext style={{margin: "9px 0 0", color: "#707070", lineheight: "16px"}}>[옵션 : 아이아이]</Tdcontentstext>
+                        </InfoTd2>
+                        <InfoTd2 style={{paddingright: "10px",borderRight:"1px solid #ebebeb",textAlign:"right"}}><Tdcontentstext style={{fontWeight:"bold"}}>22,500</Tdcontentstext><Tdcontentstext style={{fontWeight:"bold"}}>원</Tdcontentstext></InfoTd2>
+                        <InfoTd2 style={{paddingright: "10px",borderRight:"1px solid #ebebeb",paddingLeft: 0, paddingRight: 0,textAlign:"center"}}><Tdcontentstext style={{fontWeight:"bold"}}>1</Tdcontentstext></InfoTd2>
+                        <InfoTd2 style={{paddingright: "10px",borderRight:"1px solid #ebebeb",paddingLeft: 0, paddingRight: 0,textAlign:"center"}}><Tdcontentstext>-</Tdcontentstext></InfoTd2>
+                        <InfoTd2 style={{paddingright: "10px",borderRight:"1px solid #ebebeb",paddingLeft: 0, paddingRight: 0,textAlign:"center"}}><Tdcontentstext>기본배송</Tdcontentstext></InfoTd2>
+                        <InfoTd2 style={{paddingright: "10px",borderRight:"1px solid #ebebeb",paddingLeft: 0, paddingRight: 0,textAlign:"center"}}><Tdcontentstext>조건</Tdcontentstext></InfoTd2>
+                        <InfoTd2 style={{paddingright: "10px",paddingLeft: 0, paddingRight: "10ox",textAlign:"right"}}><Tdcontentstext style={{fontWeight:"bold"}}>22,500</Tdcontentstext><Tdcontentstext style={{fontWeight:"bold"}}>원</Tdcontentstext></InfoTd2>
+                    </tr>
+                    </tbody>
+                </InfoTable>
+                </OrderArea>
+            </Contents>
+        </Container>
 
 
     )
 }
 export default Basket;
+
+
+
+let Container = styled.div`  //가장 큰거 담는 부분
+    position: relative;
+    width: 100%;
+    margin: 0 auto;
+    max-width: 1460px;
+    width: 100%;
+    min-width: 1260px;
+    padding: 0 40px;
+    box-sizing: border-box;
+`
+let Contents = styled.div`  //가장 큰거 담는 부분2
+    width: 100%;
+    position: relative;
+    margin: 20px 0 0;
+    float: left;
+    min-height: 500px;;
+`
+let Path = styled.div`  //경로담기
+    margin-top: 15px;
+    overflow: hidden;
+    height: 30px;
+    line-height: 30px;
+    font-size: 10px;
+    border: 1px;
+    text-align: right;
+`
+let Title = styled.div` //제목부분
+    min-height: 30px;
+    margin: 15px 0 20px;
+    text-align: center;
+`
+
+let Titletext = styled.div` //제목스타일
+    display: inline-block;
+    padding: 0;
+    color: #757575;
+    font-size: 18px;
+    font-weight: normal;
+`
+
+let InfoTitleDiv = styled.div` //안내 바를 감쌀 디브
+    margin: 40px 0 0;   
+`
+
+let ControlInfo = styled.ul` //안내 바
+    margin: 0 0 20px;
+    border-left: 0;
+    background: #fff;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    border-bottom: 1px solid #c9c9c9;
+    width: 100%;
+    height: 44px;
+
+    position: relative;
+    font-size: 0;
+` 
+
+let ControlInfocontents = styled.li` //내용
+    border-left: 1px solid #c9c9c9;
+    border-right: 1px solid #c9c9c9;
+    border-top: 3px solid #5a5a5a;
+    border-bottom: 1px solid white;
+    background: #fff;
+    height: 41px;
+    display: inline-block;
+    position: relative;
+    font-size: 11px;
+    line-height: 37px;
+    margin: 0;
+    padding: 0 30px;
+    color: #707070;
+    text-decoration: none;
+    outline: 0;
+    text-align: center;
+    margin-top: 2px;
+   
+`
+
+let OrderArea = styled.div` //주문내역
+    display: block;
+    margin: 0;
+    padding: 0;
+`
+
+let OrderAreaTitle = styled.div ` //주문내역이름박스
+    display: flex;
+    position: relative;
+    height: 38px;
+    margin: 0;
+    padding: 0 0 0 9px;
+    border: 1px solid #ebebeb;
+    align-items: center;
+    border-bottom: 0;
+    line-height: 38px;
+    background: #f6f6f6;
+`
+
+let OrderAreaTitleContents = styled.h3 ` //이름
+    display: inline-block;
+    vertical-align: middle;
+    color: #353535;
+    font-size: 12px;
+    font-weight: bold;
+`
+
+let BeforeButton = styled.button ` //뒤로가기 버튼
+    float: right;
+    font-size: 11px;
+    line-height: 11px;
+    //display: inline-block;
+    padding: 7px 8px;
+    border: 1px solid #ebebeb;
+    border-radius: 0px;
+    background: #fff;
+    margin: 1px 0 0;
+    vertical-align: middle;
+    color : #666666;
+    -webkit-padding-before: 7px;
+    -webkit-padding-after: 8px;
+    margin-left: 85%;
+    cursor: pointer;
+
+    &:hover{  
+    border: 1px solid #CCCCCC;
+    color: #CCC;
+    }
+`
+
+
+let InfoTable = styled.table `  //정보 테이블
+    border-top: 1px solid #ebebeb;
+    line-height: 1.5;
+    position: relative;
+    width: 100%;
+    border: 1;
+    border-spacing: 0;
+    border-collapse: collapse;
+`
+
+let InfoTh = styled.th ` //정보테이블 th
+    padding: 20px 0;
+    font-size: 11px;
+    border-bottom: 1px solid #ebebeb;
+    color: #757575;
+    vertical-align: middle;
+    font-weight: normal;
+    letter-spacing: 0.5px;
+    border: 0;
+    word-break: break-all;
+    word-wrap: break-word;
+`
+
+let InfoTd = styled.td` //정보테이블 foot td
+    
+    padding: 15px 10px 17px;
+    font-size: 11px;
+    background: #fbfafa;
+    border-left: 1px solid #ebebeb;
+    border-bottom: 1px solid #ebebeb;
+    vertical-align: middle;
+    text-align: right;
+    color: #757575;
+    word-break: break-all;
+    word-wrap: break-word;
+`
+
+let Tdcontentstext = styled.text ` //텍스트스타일
+    font-size: 11px;
+    margin: 6px 0 0;
+    color: #757575;
+`
+
+let InfoTd2 = styled.td` //정보테이블 tbody td
+    padding: 15px 10px 14px;
+    border-color: #ebebeb;
+    border-top: 1px solid #ebebeb;
+    border-bottom: 1px solid #ebebeb;
+    color: #757575;
+    vertical-align: middle;
+    word-break: break-all;
+    word-wrap: break-word;
+` 
+
+let Forimg = styled.img` //img
+    max-width: 75px;
+    vertical-align: middle;
+    margin: 0 2px;
+    border: none;
+`
