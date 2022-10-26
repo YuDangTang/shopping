7e6067de13529f19101f81cfcc83d68e45c8a8f7
pshhhhhhhhhh <winki93@naver.com>
2022-10-27 00:22:32 +0900

    장바구니 이용안내 수정
    <li>부분 이미지 -> 하이푼으로 변경

--- frontend/src/pages/order/Basket.js
+++ frontend/src/pages/order/Basket.js
@@ -99,18 +99,18 @@ function Basket(){
                 <InfoDivContents>
                 <InfoDivh4>장바구니 이용안내</InfoDivh4>
                 <InfoDivol>
-                <li style={{backgroundPosition: "-484px 0", padding: "0 0 0 25px", background: "url(//img.echosting.cafe24.com/skin/base/common/ico_number.png) no-repeat",color: "#707070",listStyleType: "none",fontSize:"11px"}}>해외배송 상품과 국내배송 상품은 함께 결제하실 수 없으니 장바구니 별로 따로 결제해 주시기 바랍니다.</li>
-                <li style={{backgroundPosition: "-434px -100px", padding: "0 0 0 25px", background: "url(//img.echosting.cafe24.com/skin/base/common/ico_number.png) no-repeat",color: "#707070",listStyleType: "none",fontSize:"11px"}}>해외배송 가능 상품의 경우 국내배송 장바구니에 담았다가 해외배송 장바구니로 이동하여 결제하실 수 있습니다.</li>
-                <li style={{backgroundPositionX: "-384px",backgroundPositionY: "-200px",padding: "0 0 0 25px", background: "url(//img.echosting.cafe24.com/skin/base/common/ico_number.png) no-repeat",color: "#707070",listStyleType: "none",fontSize:"11px"}}>선택하신 상품의 수량을 변경하시려면 수량변경 후 [변경] 버튼을 누르시면 됩니다.</li>
-                <li style={{backgroundPositionX: "-334px",backgroundPositionY: "-300px",padding: "0 0 0 25px", background: "url(//img.echosting.cafe24.com/skin/base/common/ico_number.png) no-repeat",color: "#707070",listStyleType: "none",fontSize:"11px"}}>[쇼핑계속하기] 버튼을 누르시면 쇼핑을 계속 하실 수 있습니다.</li>
-                <li style={{backgroundPositionX: "-284px",backgroundPositionY: "-400px",padding: "0 0 0 25px", background: "url(//img.echosting.cafe24.com/skin/base/common/ico_number.png) no-repeat",color: "#707070",listStyleType: "none",fontSize:"11px"}}>장바구니와 관심상품을 이용하여 원하시는 상품만 주문하거나 관심상품으로 등록하실 수 있습니다.</li>
-                <li style={{backgroundPositionX: "-234px",backgroundPositionY: "-500px",padding: "0 0 0 25px", background: "url(//img.echosting.cafe24.com/skin/base/common/ico_number.png) no-repeat",color: "#707070",listStyleType: "none",fontSize:"11px"}}>파일첨부 옵션은 동일상품을 장바구니에 추가할 경우 마지막에 업로드 한 파일로 교체됩니다.</li>
+                <li style={{color: "#707070",listStyleType: "none",fontSize:"11px"}}> - 해외배송 상품과 국내배송 상품은 함께 결제하실 수 없으니 장바구니 별로 따로 결제해 주시기 바랍니다.</li>
+                <li style={{color: "#707070",listStyleType: "none",fontSize:"11px"}}> - 해외배송 가능 상품의 경우 국내배송 장바구니에 담았다가 해외배송 장바구니로 이동하여 결제하실 수 있습니다.</li>
+                <li style={{color: "#707070",listStyleType: "none",fontSize:"11px"}}> - 선택하신 상품의 수량을 변경하시려면 수량변경 후 [변경] 버튼을 누르시면 됩니다.</li>
+                <li style={{color: "#707070",listStyleType: "none",fontSize:"11px"}}> - [쇼핑계속하기] 버튼을 누르시면 쇼핑을 계속 하실 수 있습니다.</li>
+                <li style={{color: "#707070",listStyleType: "none",fontSize:"11px"}}> - 장바구니와 관심상품을 이용하여 원하시는 상품만 주문하거나 관심상품으로 등록하실 수 있습니다.</li>
+                <li style={{color: "#707070",listStyleType: "none",fontSize:"11px"}}> - 파일첨부 옵션은 동일상품을 장바구니에 추가할 경우 마지막에 업로드 한 파일로 교체됩니다.</li>
                 </InfoDivol>
                 <InfoDivh4>무이자할부 이용안내</InfoDivh4>
                 <InfoDivol>
-                <li style={{backgroundPositionX: "-484px", backgroundPositionY:"0", padding: "0 0 0 25px", background: "url(//img.echosting.cafe24.com/skin/base/common/ico_number.png) no-repeat",color: "#707070",listStyleType: "none",fontSize:"11px"}}>상품별 무이자할부 혜택을 받으시려면 무이자할부 상품만 선택하여 [주문하기] 버튼을 눌러 주문/결제 하시면 됩니다.</li>
-                <li style={{backgroundPositionX: "-434px", backgroundPositionY:"-100px", padding: "0 0 0 25px", background: "url(//img.echosting.cafe24.com/skin/base/common/ico_number.png) no-repeat",color: "#707070",listStyleType: "none",fontSize:"11px"}}>[전체 상품 주문] 버튼을 누르시면 장바구니의 구분없이 선택된 모든 상품에 대한 주문/결제가 이루어집니다.</li>
-                <li style={{backgroundPositionX: "-384px", backgroundPositionY:"-200px",padding: "0 0 0 25px", background: "url(//img.echosting.cafe24.com/skin/base/common/ico_number.png) no-repeat",color: "#707070",listStyleType: "none",fontSize:"11px"}}>단, 전체 상품을 주문/결제하실 경우, 상품별 무이자할부 혜택을 받으실 수 없습니다.</li>
+                <li style={{color: "#707070",listStyleType: "none",fontSize:"11px"}}> - 상품별 무이자할부 혜택을 받으시려면 무이자할부 상품만 선택하여 [주문하기] 버튼을 눌러 주문/결제 하시면 됩니다.</li>
+                <li style={{color: "#707070",listStyleType: "none",fontSize:"11px"}}> - [전체 상품 주문] 버튼을 누르시면 장바구니의 구분없이 선택된 모든 상품에 대한 주문/결제가 이루어집니다.</li>
+                <li style={{color: "#707070",listStyleType: "none",fontSize:"11px"}}> - 단, 전체 상품을 주문/결제하실 경우, 상품별 무이자할부 혜택을 받으실 수 없습니다.</li>
                 </InfoDivol>
                 </InfoDivContents>
                 </InfoDiv>
