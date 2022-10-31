fdd0fd59c3b067bb850d337ed2dc421b90b24eb2
Gabal Gim <97859625+KIN0-0@users.noreply.github.com>
2022-10-31 16:21:00 +0900

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { 
+import {
   BrowserRouter as Router,
   Routes,
   Route,
@@ -12,26 +12,24 @@ import RegisterProSize from "./pages/admin/RegisterProSize.js";
 import RegisterProMat from "./pages/admin/RegisterProMat.js";
 import RegisterMaterial from "./pages/admin/material/RegisterMaterial.js";
 import RegisterColor from "./pages/admin/material/RegisterColor.js";
-import Stocks from "./pages/admin/Stocks.js";
-import Update from "./pages/admin/update/Update.js";
-import UpdateSize from "./pages/admin/update/UpdateSize.js"; 
+import Stocks from "./pages/admin/Stocks";
+import Sales from "./pages/admin/Sales";
+import DetailUpdate from "./pages/admin/UpdateDetail.js";
 
 //conmponent importing
 import TopBox from './components/TopBox';
 import GlobalStyles from './css/GlobalStyles';
-
 import Botbox from "./components/Botbox.js";
 
 //page importing
 import Main from "./pages/Main.js";
 import Admin from "./pages/admin/Admin.js";
-import Login from"./pages/member/Login";
+import Login from "./pages/member/Login";
 import Join from "./pages/member/Join";
 import Basket from "./pages/order/Basket";
 import Order from "./pages/myshop/Order";
 import MyShop from "./pages/myshop/MyShop";
 import Product from "./pages/product/product.js";
-import Modify from './pages/member/Modify.js';
 
 function App() {
   // 로그인 상태 관리
@@ -52,31 +50,33 @@ function App() {
   return (
     <>
       <Router>
-        <GlobalStyles/>
-        <TopBox/>
-          <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/member/login" element={<Login />} />
-              <Route path="/member/Join" element={<Join />} />
-              <Route path="/member/Join" element={<Modify/>}/>
-              <Route path="/order/Basket" element={<Basket />} />
-              <Route path="/myshop/Order" element={<Order />} />
-              <Route path="/myshop/MyShop" element={<MyShop />} />
-              <Route path="/product/:id" element={<Product />} /> 
+        <GlobalStyles />
+        <TopBox />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/member/login" element={<Login />} />
+          <Route path="/member/Join" element={<Join />} />
+          <Route path="/order/Basket" element={<Basket />} />
+          <Route path="/myshop/Order" element={<Order />} />
+          <Route path="/myshop/MyShop" element={<MyShop />} />
+          <Route path="/product/:id" element={<Product />} />
 
-              <Route path="/admin" element={<Admin />} />
-              <Route path="/admin/regProName" element={<RegisterProName />}></Route>
-              <Route path="/admin/regProSzie" element={<RegisterProSize />}></Route>
-              <Route path="/admin/regProDetail" element={<RegisterProMat />}></Route>
-              <Route path="/admin/regProMaterial" element={<RegisterMaterial />}></Route>
-              <Route path="/admin/regProColor" element={<RegisterColor/>}></Route>
-              <Route path="/admin/:id/stocks" element={<Stocks/>}></Route>
-              <Route path="/admin/:id/update" element={<Update url={"update"}/>}></Route>
-              <Route path="/admin/:id/update2" element={<Update url={"update2"}/>}></Route>
-          </Routes>
-          <Botbox/>
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/regProName" element={<RegisterProName />}></Route>
+          <Route path="/admin/regProSzie" element={<RegisterProSize />}></Route>
+          <Route path="/admin/regProDetail" element={<RegisterProMat />}></Route>
+          <Route path="/admin/material/regProMaterial" element={<RegisterMaterial />}></Route>
+          <Route path="/admin/material/regProColor" element={<RegisterColor />}></Route>
+          <Route path="/admin/stocks" element={<Stocks />}></Route>
+          <Route path="/admin/sales" element={<Sales />}></Route>
+          <Route path="/admin/:id/detailUpdate" element={<DetailUpdate />}></Route>
+        </Routes>
+        <Botbox />
       </Router>
     </>
   );
 };
+
 export default App;
+
+//리셋 스타일드를 만들어서 페이지에만적용
